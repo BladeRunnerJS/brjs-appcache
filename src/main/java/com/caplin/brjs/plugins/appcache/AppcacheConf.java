2aa613f30aa5b7d7b6fb08223cc5c0fba98765ec
Andrew Lyons <andrew.lyons@caplin.com>
2014-04-09 16:08:19 +0100

    Moving confif to a "conf" folder under the aspect

--- src/main/java/com/caplin/brjs/plugins/appcache/AppcacheConf.java
+++ src/main/java/com/caplin/brjs/plugins/appcache/AppcacheConf.java
@@ -14,7 +14,7 @@ public class AppcacheConf extends ConfFile<YamlAppcacheConf>
 
 	public AppcacheConf(BRJSNode node) throws ConfigException
 	{
-		super(node, YamlAppcacheConf.class, node.file("appcache.conf"));
+		super(node, YamlAppcacheConf.class, node.file("conf/appcache.conf"));
 	}
 
 	public String getVersion()
